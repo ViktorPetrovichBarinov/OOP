152cfadecb22009c597c2b702f1e5a224648bda7
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2023-10-18 21:19:43 +0700

    правильный порядок импортов

--- Task_1_2_1/src/test/java/ru/nsu/chudinov/TreeTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/chudinov/TreeTest.java
@@ -1,13 +1,12 @@
 package ru.nsu.chudinov;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class TreeTest {
   @Test
