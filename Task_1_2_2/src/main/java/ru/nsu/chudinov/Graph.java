03175792824eb13dfbb78e04709cc6a665c638e9
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2023-11-02 10:33:36 +0700

    алгоритм Дейкстры

--- Task_1_2_2/src/main/java/ru/nsu/chudinov/Graph.java
+++ Task_1_2_2/src/main/java/ru/nsu/chudinov/Graph.java
@@ -1,6 +1,8 @@
 package ru.nsu.chudinov;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 
 public abstract class Graph<T> {
@@ -20,6 +22,63 @@ public abstract class Graph<T> {
 
     public abstract Boolean changeEdge(Edge<T> was, Edge<T> became);
 
+    public ArrayList<Vertex<T>> shortestPath(Vertex<T> vertex) {
+        int vertexLength = vertexList.size();
+        int[] distance = new int[vertexLength];
+        Arrays.fill(distance, Integer.MAX_VALUE);
+        distance[vertexList.indexOf(vertex)] = 0;
+        boolean[] mark = new boolean[vertexLength];
+        Arrays.fill(mark, false);
+
+        for (int i = 0; i < vertexLength; i++) {
+            int shortest = -1;
+            for (int j = 0; j < vertexLength; j++) {
+                if (!mark[j] && (shortest == -1 || distance[shortest] > distance[j])) {
+                    shortest = j;
+                }
+            }
+
+            if (distance[shortest] == Integer.MAX_VALUE) {
+                break;
+            }
+            mark[shortest] = true;
+
+            ArrayList<Edge<T>> adjacentEdge = getEdge(this.vertexList.get(shortest));
+            int edgeLen = adjacentEdge.size();
+            for (int j = 0; j < edgeLen; j++) {
+                Edge<T> cur = adjacentEdge.get(j);
+                if (distance[shortest] + cur.getWeight()
+                        < distance[this.vertexList.indexOf(cur.getEndingVertex())]) {
+                    distance[this.vertexList.indexOf(cur.getEndingVertex())]
+                            = distance[shortest] + cur.getWeight();
+                }
+            }
+        }
+
+        ArrayList<ConnectedVertex<T>> sortArray = new ArrayList<>();
+        for (int i = 0; i < vertexLength; i++) {
+            sortArray.add(new ConnectedVertex<>(this.vertexList.get(i), distance[i]));
+        }
+        Collections.sort(sortArray);
+        ArrayList<Vertex<T>> result = new ArrayList<>();
+        int len = sortArray.size();
+        for (int i = 0; i < len; i++) {
+            result.add(sortArray.get(i).vertex);
+        }
+        return result;
+    }
+
+
+    public ArrayList<Edge<T>> getEdge(Vertex<T> vertex) {
+        ArrayList<Edge<T>> result = new ArrayList<>();
+        int len = this.edgeList.size();
+        for (int i = 0; i < len; i++) {
+            if (this.edgeList.get(i).getStartingVertex().equals(vertex)) {
+                result.add(this.edgeList.get(i));
+            }
+        }
+        return result;
+    }
 
 
 
