e64b56bbf2208ef134cf0e5e43d8064e30c49c72
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2023-11-22 00:10:55 +0700

    добавил зависимости

--- Task_1_5_2/build.gradle.kts
+++ Task_1_5_2/build.gradle.kts
@@ -10,9 +10,18 @@ repositories {
     mavenCentral()
 }
 
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-core
+    implementation("com.fasterxml.jackson.core:jackson-core:2.16.0")
+    // https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-annotations
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.16.0")
+
 }
 
 tasks.test {
--- Task_1_5_2/src/main/java/org/example/Main.java
+++ Task_1_5_2/src/main/java/org/example/Main.java
@@ -1,7 +1,28 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Ваша строка
+        String yourString = "Test string";
+
+        // Создаем объект ObjectMapper
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            // Преобразуем строку в JSON-объект
+            Object json = objectMapper.readValue(yourString, Object.class);
+
+            // Записываем JSON-объект в файл
+            objectMapper.writeValue(new File("output.json"), json);
+
+            System.out.println("JSON успешно записан в файл 'output.json'");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
