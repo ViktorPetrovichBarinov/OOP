2fe79aeedb06ce5d568d10fc575bfd4e8bd9a5e7
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2023-11-30 00:29:46 +0700

    Теперь точно все тесты)

--- Task_1_5_1/src/test/java/org/example/PrefixCalculatorTest.java
+++ Task_1_5_1/src/test/java/org/example/PrefixCalculatorTest.java
@@ -352,175 +352,175 @@ public class PrefixCalculatorTest {
 
     }
 
-//    @Test
-//    @DisplayName("Operations then numbers then operations SOLID")
-//    void test4Solid() {
-//        String inputString = "pow log sin 3 2 1 1 2 3 + - \n";
-//        ByteArrayInputStream inputStream = new ByteArrayInputStream(inputString.getBytes());
-//        System.setIn(inputStream);
-//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//        PrintStream printStream = new PrintStream(byteArrayOutputStream);
-//        System.setOut(printStream);
-//
-//        SolidPrefixCalculator.ConsoleExpressionReader expressionReader =
-//                new SolidPrefixCalculator.ConsoleExpressionReader();
-//        SolidPrefixCalculator.OperationFactory operationFactory =
-//                new SolidPrefixCalculator.DefaultOperationFactory();
-//        ExpressionProcessor expressionProcessor = new ExpressionProcessor(expressionReader, operationFactory);
-//        expressionProcessor.calculate();
-//
-//        String outputString = byteArrayOutputStream.toString();
-//        String answer = "Enter your expression in 1 string and press \"Enter\".\n"
-//                + "If you want to exit press \"Ctrl + D\".\n"
-//                + "Incorrect input expression\n"
-//                + "Enter your expression in 1 string and press \"Enter\".\n"
-//                + "If you want to exit press \"Ctrl + D\".\n"
-//                + "The end.\n";
-//
-//        assertEquals(answer, outputString);
-//
-//        System.setIn(System.in);
-//        System.setOut(new PrintStream(System.out));
-//
-//    }
-//
-//    @Test
-//    @DisplayName("Division and multiplication SOLID")
-//    void test5Solid() {
-//        String inputString = "* / 6 3 7\n";
-//        ByteArrayInputStream inputStream = new ByteArrayInputStream(inputString.getBytes());
-//        System.setIn(inputStream);
-//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//        PrintStream printStream = new PrintStream(byteArrayOutputStream);
-//        System.setOut(printStream);
-//
-//        SolidPrefixCalculator.ConsoleExpressionReader expressionReader =
-//                new SolidPrefixCalculator.ConsoleExpressionReader();
-//        SolidPrefixCalculator.OperationFactory operationFactory =
-//                new SolidPrefixCalculator.DefaultOperationFactory();
-//        ExpressionProcessor expressionProcessor = new ExpressionProcessor(expressionReader, operationFactory);
-//        expressionProcessor.calculate();
-//
-//        String outputString = byteArrayOutputStream.toString();
-//        String answer = "Enter your expression in 1 string and press \"Enter\".\n"
-//                + "If you want to exit press \"Ctrl + D\".\n"
-//                + "result : 14.0\n"
-//                + "Enter your expression in 1 string and press \"Enter\".\n"
-//                + "If you want to exit press \"Ctrl + D\".\n"
-//                + "The end.\n";
-//
-//        assertEquals(answer, outputString);
-//
-//        System.setIn(System.in);
-//        System.setOut(new PrintStream(System.out));
-//
-//    }
-//
-//    @Test
-//    @DisplayName("sin cos SOLID")
-//    void test6Solid() {
-//        String inputString = "sin 3.14\n"
-//                + "cos 3.14\n";
-//        ByteArrayInputStream inputStream = new ByteArrayInputStream(inputString.getBytes());
-//        System.setIn(inputStream);
-//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//        PrintStream printStream = new PrintStream(byteArrayOutputStream);
-//        System.setOut(printStream);
-//
-//        SolidPrefixCalculator.ConsoleExpressionReader expressionReader =
-//                new SolidPrefixCalculator.ConsoleExpressionReader();
-//        SolidPrefixCalculator.OperationFactory operationFactory =
-//                new SolidPrefixCalculator.DefaultOperationFactory();
-//        ExpressionProcessor expressionProcessor = new ExpressionProcessor(expressionReader, operationFactory);
-//        expressionProcessor.calculate();
-//
-//        String outputString = byteArrayOutputStream.toString();
-//        String answer = "Enter your expression in 1 string and press \"Enter\".\n"
-//                + "If you want to exit press \"Ctrl + D\".\n"
-//                + "result : 0.0015926529164868282\n"
-//                + "Enter your expression in 1 string and press \"Enter\".\n"
-//                + "If you want to exit press \"Ctrl + D\".\n"
-//                + "result : -0.9999987317275395\n"
-//                + "Enter your expression in 1 string and press \"Enter\".\n"
-//                + "If you want to exit press \"Ctrl + D\".\n"
-//                + "The end.\n";
-//
-//        assertEquals(answer, outputString);
-//
-//        System.setIn(System.in);
-//        System.setOut(new PrintStream(System.out));
-//
-//    }
-//
-//    @Test
-//    @DisplayName("pow sqrt SOLID")
-//    void test7Solid() {
-//        String inputString = "sqrt pow 5 2\n"
-//                + "log 2 8\n";
-//        ByteArrayInputStream inputStream = new ByteArrayInputStream(inputString.getBytes());
-//        System.setIn(inputStream);
-//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//        PrintStream printStream = new PrintStream(byteArrayOutputStream);
-//        System.setOut(printStream);
-//
-//        SolidPrefixCalculator.ConsoleExpressionReader expressionReader =
-//                new SolidPrefixCalculator.ConsoleExpressionReader();
-//        SolidPrefixCalculator.OperationFactory operationFactory =
-//                new SolidPrefixCalculator.DefaultOperationFactory();
-//        ExpressionProcessor expressionProcessor = new ExpressionProcessor(expressionReader, operationFactory);
-//        expressionProcessor.calculate();
-//
-//        String outputString = byteArrayOutputStream.toString();
-//        String answer = "Enter your expression in 1 string and press \"Enter\".\n"
-//                + "If you want to exit press \"Ctrl + D\".\n"
-//                + "result : 5.0\n"
-//                + "Enter your expression in 1 string and press \"Enter\".\n"
-//                + "If you want to exit press \"Ctrl + D\".\n"
-//                + "result : 3.0\n"
-//                + "Enter your expression in 1 string and press \"Enter\".\n"
-//                + "If you want to exit press \"Ctrl + D\".\n"
-//                + "The end.\n";
-//
-//        assertEquals(answer, outputString);
-//
-//        System.setIn(System.in);
-//        System.setOut(new PrintStream(System.out));
-//
-//    }
-//
-//    @Test
-//    @DisplayName("incorrect input then correct input SOLID")
-//    void test8Solid() {
-//        String inputString = "sqrt pow 5 2 7 8 32.2 88 1\n"
-//                + "log 2 8\n";
-//        ByteArrayInputStream inputStream = new ByteArrayInputStream(inputString.getBytes());
-//        System.setIn(inputStream);
-//        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-//        PrintStream printStream = new PrintStream(byteArrayOutputStream);
-//        System.setOut(printStream);
-//
-//        SolidPrefixCalculator.ConsoleExpressionReader expressionReader =
-//                new SolidPrefixCalculator.ConsoleExpressionReader();
-//        SolidPrefixCalculator.OperationFactory operationFactory =
-//                new SolidPrefixCalculator.DefaultOperationFactory();
-//        ExpressionProcessor expressionProcessor = new ExpressionProcessor(expressionReader, operationFactory);
-//        expressionProcessor.calculate();
-//
-//        String outputString = byteArrayOutputStream.toString();
-//        String answer = "Enter your expression in 1 string and press \"Enter\".\n"
-//                + "If you want to exit press \"Ctrl + D\".\n"
-//                + "Incorrect input expression\n"
-//                + "Enter your expression in 1 string and press \"Enter\".\n"
-//                + "If you want to exit press \"Ctrl + D\".\n"
-//                + "result : 3.0\n"
-//                + "Enter your expression in 1 string and press \"Enter\".\n"
-//                + "If you want to exit press \"Ctrl + D\".\n"
-//                + "The end.\n";
-//
-//        assertEquals(answer, outputString);
-//
-//        System.setIn(System.in);
-//        System.setOut(new PrintStream(System.out));
-//
-//    }
+    @Test
+    @DisplayName("Operations then numbers then operations SOLID")
+    void test4Solid() {
+        String inputString = "pow log sin 3 2 1 1 2 3 + - \n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(inputString.getBytes());
+        System.setIn(inputStream);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+        System.setOut(printStream);
+
+        SolidPrefixCalculator.ConsoleExpressionReader expressionReader =
+                new SolidPrefixCalculator.ConsoleExpressionReader();
+        SolidPrefixCalculator.OperationFactory operationFactory =
+                new SolidPrefixCalculator.DefaultOperationFactory();
+        ExpressionProcessor expressionProcessor = new ExpressionProcessor(expressionReader, operationFactory);
+        expressionProcessor.calculate();
+
+        String outputString = byteArrayOutputStream.toString();
+        String answer = "Enter your expression in 1 string and press \"Enter\".\n"
+                + "If you want to exit press \"Ctrl + D\".\n"
+                + "Incorrect input expression\n"
+                + "Enter your expression in 1 string and press \"Enter\".\n"
+                + "If you want to exit press \"Ctrl + D\".\n"
+                + "The end.\n";
+
+        assertEquals(answer, outputString);
+
+        System.setIn(System.in);
+        System.setOut(new PrintStream(System.out));
+
+    }
+
+    @Test
+    @DisplayName("Division and multiplication SOLID")
+    void test5Solid() {
+        String inputString = "* / 6 3 7\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(inputString.getBytes());
+        System.setIn(inputStream);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+        System.setOut(printStream);
+
+        SolidPrefixCalculator.ConsoleExpressionReader expressionReader =
+                new SolidPrefixCalculator.ConsoleExpressionReader();
+        SolidPrefixCalculator.OperationFactory operationFactory =
+                new SolidPrefixCalculator.DefaultOperationFactory();
+        ExpressionProcessor expressionProcessor = new ExpressionProcessor(expressionReader, operationFactory);
+        expressionProcessor.calculate();
+
+        String outputString = byteArrayOutputStream.toString();
+        String answer = "Enter your expression in 1 string and press \"Enter\".\n"
+                + "If you want to exit press \"Ctrl + D\".\n"
+                + "result : 14.0\n"
+                + "Enter your expression in 1 string and press \"Enter\".\n"
+                + "If you want to exit press \"Ctrl + D\".\n"
+                + "The end.\n";
+
+        assertEquals(answer, outputString);
+
+        System.setIn(System.in);
+        System.setOut(new PrintStream(System.out));
+
+    }
+
+    @Test
+    @DisplayName("sin cos SOLID")
+    void test6Solid() {
+        String inputString = "sin 3.14\n"
+                + "cos 3.14\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(inputString.getBytes());
+        System.setIn(inputStream);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+        System.setOut(printStream);
+
+        SolidPrefixCalculator.ConsoleExpressionReader expressionReader =
+                new SolidPrefixCalculator.ConsoleExpressionReader();
+        SolidPrefixCalculator.OperationFactory operationFactory =
+                new SolidPrefixCalculator.DefaultOperationFactory();
+        ExpressionProcessor expressionProcessor = new ExpressionProcessor(expressionReader, operationFactory);
+        expressionProcessor.calculate();
+
+        String outputString = byteArrayOutputStream.toString();
+        String answer = "Enter your expression in 1 string and press \"Enter\".\n"
+                + "If you want to exit press \"Ctrl + D\".\n"
+                + "result : 0.0015926529164868282\n"
+                + "Enter your expression in 1 string and press \"Enter\".\n"
+                + "If you want to exit press \"Ctrl + D\".\n"
+                + "result : -0.9999987317275395\n"
+                + "Enter your expression in 1 string and press \"Enter\".\n"
+                + "If you want to exit press \"Ctrl + D\".\n"
+                + "The end.\n";
+
+        assertEquals(answer, outputString);
+
+        System.setIn(System.in);
+        System.setOut(new PrintStream(System.out));
+
+    }
+
+    @Test
+    @DisplayName("pow sqrt SOLID")
+    void test7Solid() {
+        String inputString = "sqrt pow 5 2\n"
+                + "log 2 8\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(inputString.getBytes());
+        System.setIn(inputStream);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+        System.setOut(printStream);
+
+        SolidPrefixCalculator.ConsoleExpressionReader expressionReader =
+                new SolidPrefixCalculator.ConsoleExpressionReader();
+        SolidPrefixCalculator.OperationFactory operationFactory =
+                new SolidPrefixCalculator.DefaultOperationFactory();
+        ExpressionProcessor expressionProcessor = new ExpressionProcessor(expressionReader, operationFactory);
+        expressionProcessor.calculate();
+
+        String outputString = byteArrayOutputStream.toString();
+        String answer = "Enter your expression in 1 string and press \"Enter\".\n"
+                + "If you want to exit press \"Ctrl + D\".\n"
+                + "result : 5.0\n"
+                + "Enter your expression in 1 string and press \"Enter\".\n"
+                + "If you want to exit press \"Ctrl + D\".\n"
+                + "result : 3.0\n"
+                + "Enter your expression in 1 string and press \"Enter\".\n"
+                + "If you want to exit press \"Ctrl + D\".\n"
+                + "The end.\n";
+
+        assertEquals(answer, outputString);
+
+        System.setIn(System.in);
+        System.setOut(new PrintStream(System.out));
+
+    }
+
+    @Test
+    @DisplayName("incorrect input then correct input SOLID")
+    void test8Solid() {
+        String inputString = "sqrt pow 5 2 7 8 32.2 88 1\n"
+                + "log 2 8\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(inputString.getBytes());
+        System.setIn(inputStream);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+        System.setOut(printStream);
+
+        SolidPrefixCalculator.ConsoleExpressionReader expressionReader =
+                new SolidPrefixCalculator.ConsoleExpressionReader();
+        SolidPrefixCalculator.OperationFactory operationFactory =
+                new SolidPrefixCalculator.DefaultOperationFactory();
+        ExpressionProcessor expressionProcessor = new ExpressionProcessor(expressionReader, operationFactory);
+        expressionProcessor.calculate();
+
+        String outputString = byteArrayOutputStream.toString();
+        String answer = "Enter your expression in 1 string and press \"Enter\".\n"
+                + "If you want to exit press \"Ctrl + D\".\n"
+                + "Incorrect input expression\n"
+                + "Enter your expression in 1 string and press \"Enter\".\n"
+                + "If you want to exit press \"Ctrl + D\".\n"
+                + "result : 3.0\n"
+                + "Enter your expression in 1 string and press \"Enter\".\n"
+                + "If you want to exit press \"Ctrl + D\".\n"
+                + "The end.\n";
+
+        assertEquals(answer, outputString);
+
+        System.setIn(System.in);
+        System.setOut(new PrintStream(System.out));
+
+    }
 }
