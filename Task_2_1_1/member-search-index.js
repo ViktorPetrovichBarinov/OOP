eb0f0ecefda64f6239ae11c7cb1e02441450b2d5
ViktorPetrovichBarinov <ViktorPetrovichBarinov@users.noreply.github.com>
2024-02-07 03:59:35 +0000

    Deploying to gh-pages from @ ViktorPetrovichBarinov/OOP@75df4f4671c4cb852e65dc66c862c1c1ab77fb25 🚀

---
+++ Task_2_1_1/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"org.example","c":"Logic","l":"Logic()","url":"%3Cinit%3E()"},{"p":"org.example","c":"Main","l":"Main()","url":"%3Cinit%3E()"},{"p":"org.example","c":"Main","l":"main(String[])","url":"main(java.lang.String[])"},{"p":"org.example","c":"Logic","l":"streamCalculation(long[], int)","url":"streamCalculation(long[],int)"}]
