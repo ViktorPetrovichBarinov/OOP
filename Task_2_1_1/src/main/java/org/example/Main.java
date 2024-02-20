25ea2d9870dceaeb3e5ebc852bcb574e48e5e208
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2024-02-21 02:08:43 +0700

    график

--- Task_2_1_1/src/main/java/org/example/Main.java
+++ Task_2_1_1/src/main/java/org/example/Main.java
@@ -33,6 +33,29 @@ public class Main {
             1000000000000000877L,
             1000000000000000913L,
             1000000000000000931L,
+            1000000000000000997L,
+            1000000000000000003L,
+            1000000000000000009L,
+            1000000000000000031L,
+            1000000000000000079L,
+            1000000000000000177L,
+            1000000000000000183L,
+            1000000000000000201L,
+            1000000000000000283L,
+            1000000000000000381L,
+            1000000000000000387L,
+            1000000000000000507L,
+            1000000000000000523L,
+            1000000000000000583L,
+            1000000000000000603L,
+            1000000000000000619L,
+            1000000000000000621L,
+            1000000000000000799L,
+            1000000000000000841L,
+            1000000000000000861L,
+            1000000000000000877L,
+            1000000000000000913L,
+            1000000000000000931L,
             1000000000000000997L};
 
         long startTime;
@@ -45,7 +68,7 @@ public class Main {
         System.out.println("  ||  " + (endTime - startTime));
 
         ParallelCalculation parallelCalculation = new ParallelCalculation(1);
-        for (int i = 1; i <= 4; i++) {
+        for (int i = 1; i <= 6; i++) {
             parallelCalculation.setNumberOfThreads(i);
             startTime = System.currentTimeMillis();
             System.out.print("thread calculus(" + i + "): " + parallelCalculation.calculate(array));
@@ -54,7 +77,7 @@ public class Main {
         }
 
         StreamCalculation streamCalculation = new StreamCalculation(1);
-        streamCalculation.setNumberOfThreads(4);
+        streamCalculation.setNumberOfThreads(6);
         startTime = System.currentTimeMillis();
         System.out.print("stream calculus: " + streamCalculation.calculate(array));
         endTime = System.currentTimeMillis();
