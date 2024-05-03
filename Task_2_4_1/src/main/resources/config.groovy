76367c0d503437d9202acd7997058e564e9d5c51
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2024-05-04 02:38:40 +0700

    + конфиг

---
+++ Task_2_4_1/src/main/resources/config.groovy
@@ -0,0 +1,10 @@
+students {
+    "student1" {
+        username = "Ruslan Chudinov"
+        repository = "https://github.com/ViktorPetrovichBarinov/OOP"
+    }
+    "student2" {
+        username = "Rustam Khamidullin"
+        repository = "https://github.com/Rustam-Khamidullin/OOP"
+    }
+}
