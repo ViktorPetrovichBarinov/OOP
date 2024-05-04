8db2bdcdf294c847b8902893a68590c183af6500
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2024-05-05 02:41:20 +0700

    оно этого, того самого, завелось

--- Task_2_4_1/src/main/resources/scripts/cloneRepositories.groovy
+++ Task_2_4_1/src/main/resources/scripts/cloneRepositories.groovy
@@ -9,5 +9,20 @@ def config = new GroovyClassLoader().parseClass(
 def cloneRepos(LinkedHashMap students) {
     println 'user list:' + students.keySet()
 
+    for (id in students.keySet()) {
+        print id
+        pathToCloning = "repositories/${id}"
+        println (" " + pathToCloning)
+
+        student = students.get(id)
+        println(student.repository)
+
+        def clonning = ('git clone ' + student.repository + ' ' + pathToCloning).execute()
+        def help = new StringBuffer()
+        clonning.consumeProcessErrorStream(help)
+        println help.toString()
+
+    }
+
 }
 cloneRepos(config.students)
