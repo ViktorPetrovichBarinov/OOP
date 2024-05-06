94d46b9262216d75dd9b0d32a45ead2b127825bc
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2024-05-07 05:14:12 +0700

    добавил билд + тесты + джавадоки

--- Task_2_4_1/build.gradle.kts
+++ Task_2_4_1/build.gradle.kts
@@ -14,7 +14,13 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 
+    implementation("org.gradle:gradle-tooling-api:7.3-20210825160000+0000")
+    runtimeOnly("org.slf4j:slf4j-simple:1.7.10")
     implementation("org.apache.groovy:groovy-all:5.0.0-alpha-1")
+    implementation("org.jsoup:jsoup:1.17.2")
+    implementation("org.freemarker:freemarker:2.3.30")
+    implementation("commons-io:commons-io:2.16.1")
+
 }
 
 tasks.test {
---
+++ Task_2_4_1/src/main/resources/scripts/buildChecker.groovy
@@ -0,0 +1,97 @@
+package scripts
+
+import org.gradle.tooling.GradleConnector
+import org.jsoup.Jsoup
+
+//Получаем конфиги для студентов и для тасок
+def engine = new GroovyScriptEngine("scripts/")
+def scriptClass = engine.loadScriptByName("tasks.groovy")
+def taskConfig = scriptClass.getDeclaredConstructor().newInstance()
+scriptClass = engine.loadScriptByName("students.groovy")
+def studentsConfig = scriptClass.getDeclaredConstructor().newInstance()
+
+def evaluate(Set students, String lab) {
+    println "-------------------"
+    println lab + ':'
+
+    def results = new HashMap()
+    for (student in students) {
+        def connector = GradleConnector.newConnector()
+
+        studentPath = new File("./repositories/" + student);
+        //println(studentPath)
+
+        println "----------"
+        println "${student}:"
+        //println "Testing:"
+
+        def studentResults = [
+                build: '-',
+                javadoc: '-',
+                test: '-'
+        ]
+
+        def labPath = "./repositories/" + student + '/' + lab
+        connector.forProjectDirectory(new File(labPath))
+        def connection = connector.connect()
+        def build = connection.newBuild()
+
+
+        //build
+        try {
+            build.forTasks('build')
+                    .run()
+            studentResults['build'] = '+'
+            println "Building +"
+        } catch (Exception e) {
+            println "Building -"
+        }
+
+        //coverage
+        try {
+            build.forTasks('test')
+                    .addArguments('-i')
+                    .run()
+            def link = labPath + '/build/reports/tests/test/index.html'
+            def testSummary = new File(link)
+            def document = Jsoup.parse(testSummary)
+
+            def value = document.getElementById("successRate").select("div.percent").first().text()
+            println 'TESTS COMPLETED + ' + value
+
+            studentResults['test'] = value
+            //studentResults['summaryHTML'] = document.getElementById("content").outerHtml()
+        } catch (Exception e) {
+            println 'TESTS FAILED - '
+        }
+
+        //docs
+        try {
+            build.forTasks('javadoc')
+                    .run()
+            studentResults['javadoc'] = '+'
+            println "JAVADOC +"
+        } catch (Exception e) {
+            println "Javadoc -"
+        }
+
+        results[student] = studentResults
+    }
+    return results;
+}
+
+
+//Клонируем репы
+def shell = new GroovyShell()
+def script = shell.parse(new File("cloneRepositories.groovy"))
+script.run()
+
+def labResults = new HashMap();
+for (lab in taskConfig.tasks) {
+    def labResult = evaluate(studentsConfig.students.keySet(), lab);
+    labResults[lab] = labResult
+}
+
+println "-------------"
+
+return labResults
