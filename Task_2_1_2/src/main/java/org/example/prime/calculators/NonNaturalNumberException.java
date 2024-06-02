6658e1ddf8b5e4a9fbab28567a760e3d1d04cadd
Ruslan Chudinov <r.chudinov@g.nsu.ru>
2024-06-03 02:20:16 +0700

    чекстайл

--- Task_2_1_2/src/main/java/org/example/prime/calculators/NonNaturalNumberException.java
+++ Task_2_1_2/src/main/java/org/example/prime/calculators/NonNaturalNumberException.java
@@ -1,9 +1,14 @@
 package org.example.prime.calculators;
 
-public class NonNaturalNumberException extends Exception{
-    NonNaturalNumberException(){
+/**
+ * Название ошибки говорит само за себя.
+ */
+public class NonNaturalNumberException extends Exception {
+
+    NonNaturalNumberException() {
         super("Number is not a natural number");
     }
+
     public NonNaturalNumberException(String message) {
         super(message);
     }
