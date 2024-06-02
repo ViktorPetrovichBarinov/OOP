f07790e8daa70f860ee252eb8136521d8c7a0bcb
ViktorPetrovichBarinov <ViktorPetrovichBarinov@users.noreply.github.com>
2024-06-02 19:13:20 +0000

    Deploying to gh-pages from @ ViktorPetrovichBarinov/OOP@cc6176eecba554ca0f8e73fc47a6af5c62c05cbd 🚀

--- Task_2_1_2/package-search-index.js
+++ Task_2_1_2/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.example"},{"l":"org.example.primeCalculators"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.example"},{"l":"org.example.prime.calculators"}]
